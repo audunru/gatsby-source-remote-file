607f1864a29d92ad0b844209f55110ec26a54c79
Audun Rundberg <audun@rundberg.no>
2020-02-18 00:28:46 +0100

    publish from dist folder, and make it the root

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,7 @@
 import typescript from 'rollup-plugin-typescript2';
 import { terser } from 'rollup-plugin-terser';
+import copy from 'rollup-plugin-copy';
+import generatePackageJson from 'rollup-plugin-generate-package-json';
 import pkg from './package.json';
 
 export default {
@@ -11,5 +13,27 @@ export default {
     },
   ],
   external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
-  plugins: [typescript(), terser()],
+  plugins: [
+    typescript(),
+    terser(),
+    copy({
+      targets: [{ src: ['LICENSE', 'README.md'], dest: 'dist' }],
+    }),
+    generatePackageJson({
+      baseContents: pkg => ({
+        name: pkg.name,
+        description: pkg.description,
+        version: pkg.version,
+        main: pkg.main.replace('dist/', ''),
+        types: pkg.main.replace('dist/', ''),
+        author: pkg.author,
+        license: pkg.license,
+        repository: pkg.repository,
+        keywords: pkg.keywords,
+        bugs: pkg.bugs,
+        homepage: pkg.homepage,
+      }),
+      additionalDependencies: ['gatsby'],
+    }),
+  ],
 };
