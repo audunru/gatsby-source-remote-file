e846442765d6fd1c977a0bdadbcbae080303f78d
Audun Rundberg <audun@rundberg.no>
2020-02-18 00:00:32 +0100

    setup rollup to use dist folder

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+import typescript from 'rollup-plugin-typescript2';
+import { terser } from 'rollup-plugin-terser';
+import pkg from './package.json';
+
+export default {
+  input: 'src/gatsby-node.ts',
+  output: [
+    {
+      file: 'dist/gatsby-node.js',
+      format: 'cjs',
+    },
+  ],
+  external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
+  plugins: [typescript(), terser()],
+};
