ad440ae5221481de769873f43c20bf581b4506ea
Audun Rundberg <audun@rundberg.no>
2020-02-18 21:28:02 +0100

    Use rollup in build process (#1)
    * setup rollup to use dist folder
    
    * Delete index.ts
    
    * dist/gatsby-node.js is main entry
    
    * publish from dist folder, and make it the root
    
    * fix setting types in package.json
    
    * use git-tag-version to update version in package json
    
    * use root as outdir for development

---
+++ rollup.config.js
@@ -0,0 +1,42 @@
+import typescript from 'rollup-plugin-typescript2';
+import { terser } from 'rollup-plugin-terser';
+import copy from 'rollup-plugin-copy';
+import generatePackageJson from 'rollup-plugin-generate-package-json';
+import gitVersion from 'git-tag-version';
+import pkg from './package.json';
+
+const tag = gitVersion();
+
+export default {
+  input: 'src/gatsby-node.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+    },
+  ],
+  external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
+  plugins: [
+    typescript(),
+    terser(),
+    copy({
+      targets: [{ src: ['LICENSE', 'README.md'], dest: 'dist' }],
+    }),
+    generatePackageJson({
+      baseContents: pkg => ({
+        name: pkg.name,
+        description: pkg.description,
+        version: tag,
+        main: pkg.main.replace('dist/', ''),
+        types: pkg.types.replace('dist/', ''),
+        author: pkg.author,
+        license: pkg.license,
+        repository: pkg.repository,
+        keywords: pkg.keywords,
+        bugs: pkg.bugs,
+        homepage: pkg.homepage,
+      }),
+      additionalDependencies: ['gatsby'],
+    }),
+  ],
+};
--- src/index.ts
+++
@@ -1,1 +0,0 @@
-export { RemotePluginOptions } from './gatsby-node';
