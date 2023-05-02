78a8c2c7d28727f2efe7ca41509e3c3750be01ed
Audun Rundberg <audun.rundberg@nhn.no>
2023-05-03 00:29:20 +0200

    build: use standard version for releases

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,7 @@ import typescript from "rollup-plugin-typescript2";
 import { terser } from "rollup-plugin-terser";
 import copy from "rollup-plugin-copy";
 import generatePackageJson from "rollup-plugin-generate-package-json";
-import gitVersion from "git-tag-version";
 import pkg from "./package.json";
-
-const tag = gitVersion();
 
 export default {
   input: "src/gatsby-node.ts",
@@ -28,7 +25,7 @@ export default {
       baseContents: (pkg) => ({
         name: pkg.name,
         description: pkg.description,
-        version: tag,
+        version: pkg.version,
         main: pkg.main.replace("dist/", ""),
         types: pkg.types.replace("dist/", ""),
         author: pkg.author,
