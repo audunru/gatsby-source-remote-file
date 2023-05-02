941813ab4aa9b65bf25c4b2c9e303e1562dacef5
Audun Rundberg <audun.rundberg@nhn.no>
2023-05-03 00:51:55 +0200

    build: replace ts-jest with babel

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,5 @@
 module.exports = {
   testMatch: ["**/__tests__/**/*.+(ts)", "**/?(*.)+(spec|test).+(ts)"],
-  transform: {
-    "^.+\\.ts?$": "ts-jest",
-  },
-  globals: {
-    "ts-jest": {
-      diagnostics: false,
-    },
-  },
   collectCoverage: true,
   coverageReporters: ["text"],
   coverageThreshold: {
